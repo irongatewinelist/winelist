1476f9ffa8a648303d7cb429f26b27a3b24dc0b0
irongatewinelist <irongatetestingwinelist@gmail.com>
2026-03-05 18:25:18 -0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+var CACHE_NAME = 'winelist-v1';
+var ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.json'];
+
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (names) {
+      return Promise.all(
+        names.filter(function (name) { return name !== CACHE_NAME; })
+            .map(function (name) { return caches.delete(name); })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    fetch(event.request)
+      .then(function (response) {
+        var clone = response.clone();
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, clone);
+        });
+        return response;
+      })
+      .catch(function () {
+        return caches.match(event.request);
+      })
+  );
+});
